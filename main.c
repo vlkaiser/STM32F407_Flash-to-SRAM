3d8a92c83b39880c239d3fdaea368948b82aea8d
Vicki <vlkaiser9@gmail.com>
2019-10-01 17:25:53 -0400

    Added: Function to copy data from FLASH memory array (const) to SRAM memory

--- main.c
+++ main.c
@@ -29,8 +29,16 @@
 #include <stdint.h>	
 
 /**************************************************************************************************************************/
+/* Constants and Globals */		// Globals are stored in SRAM, Constants are stored in Flash 
+
+//char my_data[] = "I love embedded programming";				//Stored in SRAM 0x200000000
+char const my_data[] = "I love embedded programming";		//Stored in FLASH 0x08000342  (Read-Only)
+char dest_data;
+#define BASE_ADDRESS_OF_SRAM 0x20000000
+
+/**************************************************************************************************************************/
 /* Functions */
-/**
+/** delay(void)
   \fn          void delay(void)
   \brief       Use a counter as a program wait function
   \returns		 Nothing - ?
@@ -41,17 +49,26 @@ void delay(void)
 	for(i=0; i<1000000; i++);
 }
 
+
+
 /**************************************************************************************************************************/
 /* Main */
 int main(void)
 {
 	/* Initialize: */
-	
-	/* Do this forever: */
-	while(1)
-		{	
-			
-		}
+	char data;
+	data = my_data[0];
 
+	//Copy byte-by-byte from my_data to BASE_ADDRESS_OF_SRAM
+	for (int i = 0; i< sizeof(my_data); i++)
+	{
+		*((uint8_t*)BASE_ADDRESS_OF_SRAM + i) = my_data[i];					//typecast as address: (uint8_t *)
+																																//increment each SRAM receiving location: + i
+																																//increment each FLASH byte in array [i]
+																																//dereferencing address and storing my_data[i] in SRAM Location = *(...) = xxx
+	/* Break at the for loop, and step through the code in debug mode (F10)
+      watch memory from address 0x20000000 as values are copied		*/
+	}
+	
 	return 0;
 }
